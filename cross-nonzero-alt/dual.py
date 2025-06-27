d34ea2eb575313918902e192fe21332084baae66
Edwin Chan <chchan@gatech.edu>
2025-06-27 01:17:38 -0400

    Add alternative cross-term-keeping representation

---
+++ cross-nonzero-alt/dual.py
@@ -0,0 +1,403 @@
+__all__ = [
+  'Dual', 'isclose', 'sqrt', 'cbrt', 'hypot', 'exp', 'log', 'sin', 'cos',
+  'tan', 'asin', 'acos', 'atan', 'sinh', 'cosh', 'tanh', 'asinh', 'acosh',
+  'atanh']
+
+import collections
+import functools
+import itertools
+import math
+import numbers
+import operator
+import sys
+
+def use_scalar(scalar):
+  global stype, sfrac, smath
+  if scalar == 'real':
+    stype = numbers.Real,
+    sfrac = operator.truediv
+    smath = math
+  elif scalar == 'complex':
+    import cmath
+    stype = numbers.Complex,
+    sfrac = operator.truediv
+    smath = cmath
+  elif scalar == 'symbol':
+    import sympy
+    stype = sympy.Basic, numbers.Number
+    sfrac = sympy.Rational
+    smath = sympy
+  else:
+    raise ValueError('unrecognized scalar')
+
+use_scalar('real')
+
+def drop_zeros(x):
+  return {k: v for k, v in x.items() if v != 0}
+
+class Dual(collections.UserDict):
+  token = itertools.count()
+
+  @classmethod
+  def new(cls, a, v):
+    return __class__({0: a, 1 << next(cls.token): v})
+
+  def __pos__(self):
+    return __class__(self)
+
+  def __neg__(self):
+    return __class__({k: -v for k, v in self.items()})
+
+  def __add__(self, other):
+    if isinstance(other, __class__):
+      return __class__(
+        {**self, **{k: self.get(k, 0) + v for k, v in other.items()}})
+    elif isinstance(other, stype):
+      return __class__({**self, **{0: self.get(0, 0) + other}})
+    else:
+      return NotImplemented
+
+  def __sub__(self, other):
+    if isinstance(other, __class__):
+      return __class__(
+        {**self, **{k: self.get(k, 0) - v for k, v in other.items()}})
+    elif isinstance(other, stype):
+      return __class__({**self, **{0: self.get(0, 0) - other}})
+    else:
+      return NotImplemented
+
+  def __mul__(self, other):
+    if isinstance(other, __class__):
+      x = __class__()
+      for (k1, v1), (k2, v2) in itertools.product(self.items(), other.items()):
+        if k1 & k2 == 0:
+          k, v = k1 | k2, v1 * v2
+          x[k] = x.get(k, 0) + v
+      return x
+    elif isinstance(other, stype):
+      return __class__({k: v * other for k, v in self.items()})
+    else:
+      return NotImplemented
+
+  def __truediv__(self, other):
+    if isinstance(other, __class__):
+      return self * other**-1
+    elif isinstance(other, stype):
+      return __class__({k: v / other for k, v in self.items()})
+    else:
+      return NotImplemented
+
+  def __pow__(self, other):
+    if isinstance(other, numbers.Integral):
+      return pow_int(self, other)
+    elif isinstance(other, stype + (__class__,)):
+      return exp(other * log(self))
+    else:
+      return NotImplemented
+
+  def __radd__(self, other):
+    return self + other
+
+  def __rsub__(self, other):
+    return -self + other
+
+  def __rmul__(self, other):
+    return self * other
+
+  def __rtruediv__(self, other):
+    return self**-1 * other
+
+  def __rpow__(self, other):
+    return exp(self * smath.log(other))
+
+  def __eq__(self, other):
+    if isinstance(other, __class__):
+      return drop_zeros(self) == drop_zeros(other)
+    elif isinstance(other, stype):
+      return drop_zeros(self) == drop_zeros({0: other})
+    else:
+      return NotImplemented
+
+  def __hash__(self):
+    if not (x := drop_zeros(self)) or x.keys() == {0}:
+      return hash(x.get(0, 0))
+    else:
+      return hash(tuple(sorted(x.items())))
+
+  def __round__(self, ndigits=None):
+    return __class__(drop_zeros(
+      {k: round(v, ndigits) for k, v in self.items()}))
+
+  def chop(self, *, rel_tol=1e-9, abs_tol=0):
+    if rel_tol < 0 or abs_tol < 0:
+      raise ValueError('tolerances must be non-negative')
+
+    tol = max(rel_tol * abs(self.get(0, 0)), abs_tol)
+
+    return __class__({k: v for k, v in self.items() if abs(v) >= tol})
+
+  def __repr__(self):
+    return '{}({})'.format(__class__.__name__, super().__repr__())
+
+def isclose(first, second, *, rel_tol=1e-9, abs_tol=0):
+  if not hasattr(smath, 'isclose'):
+    raise ValueError('scalar supports only exact operations')
+  if rel_tol < 0 or abs_tol < 0:
+    raise ValueError('tolerances must be non-negative')
+
+  first_dual    = isinstance(first, Dual)
+  first_scalar  = isinstance(first, stype)
+  second_dual   = isinstance(second, Dual)
+  second_scalar = isinstance(second, stype)
+
+  smath_isclose = functools.partial(
+    smath.isclose, rel_tol=rel_tol, abs_tol=abs_tol)
+  dual_isclose = functools.partial(
+    isclose, rel_tol=rel_tol, abs_tol=abs_tol)
+
+  if first_dual and second_dual:
+    return all(
+      smath_isclose(first.get(k, 0), second.get(k, 0))
+      for k in first.keys() | second.keys())
+  elif first_dual and second_scalar:
+    return dual_isclose(first, Dual({0: second}))
+  elif first_scalar and second_dual:
+    return dual_isclose(second, first)
+  elif first_scalar and second_scalar:
+    return smath_isclose(first, second)
+  elif not (first_dual or first_scalar):
+    raise TypeError(
+      'must be {}, not {}'
+      .format(format_types([Dual, *stype]), format_types([type(first)])))
+  elif not (second_dual or second_scalar):
+    raise TypeError(
+      'must be {}, not {}'
+      .format(format_types([Dual, *stype]), format_types([type(second)])))
+
+def sqrt(x):
+  return x**sfrac(1, 2)
+
+def cbrt(x):
+  return x**sfrac(1, 3)
+
+def hypot(*x):
+  return sqrt(sum(x**2 for x in x))
+
+def iter_set_bits(n):
+  p = 1
+  while n > 0:
+    if n & 1:
+      yield p
+    n >>= 1
+    p <<= 1
+
+def iter_nonempty_subsets(x):
+  x = list(x)
+  for n in range(1, len(x)+1):
+    yield from itertools.combinations(x, n)
+
+def iter_stirling(x, k):
+  # [[https://devblogs.microsoft.com/oldnewthing/20140324-00/?p=1413]]
+  if not x and k <= 0:
+    yield []
+  elif x and k > 0:
+    h, t = x[0], x[1:]
+    for d in iter_stirling(t, k-1):
+      yield [[h]] + d
+    for d in iter_stirling(t, k):
+      for i in range(k):
+        yield d[:i] + [[h] + d[i]] + d[i+1:]
+
+def func_from_series(x, fx_a, fx_b_cfnz, fx_b_cfz):
+  # this function defines a mathematical function f(x) through its power-series
+  # expansion around x == 0:
+  #   f(x) == sum(c[n] * x**n for n in itertools.count())
+  # where c[n] is the nth derivative of f evaluated at x == 0 divided by
+  # math.factorial(n)
+
+  # the code makes use of the structure common to all such power-series
+  # expansions; because of this, the function parameters do not have intuitive
+  # explanations, but are instead defined mathematically through
+  #   fx_a      == f(x[0])
+  #   fx_b_cfnz == lambda m: sum(math.perm(n, m) * c[n] * x[0]**n
+  #                            for n in itertools.count())
+  #   fx_b_cfz  == lambda m: math.factorial(m) * c[m]
+
+  s = list(iter_set_bits(functools.reduce(operator.or_, x.keys(), 0)))
+  m = len(s)
+  s = list(iter_nonempty_subsets(s))
+
+  k = list(map(sum, s))
+  fx_b = dict.fromkeys(k, 0)
+
+  a = x.get(0, 0)
+  if a != 0:
+    c = {k: x.get(k, 0) / a for k in k if k != 0}
+    fx_b_cf = [fx_b_cfnz(m) for m in range(1, m+1)]
+  else:
+    c = {k: x.get(k, 0) for k in k if k != 0}
+    fx_b_cf = [fx_b_cfz(m) for m in range(1, m+1)]
+
+  for s in s:
+    k = sum(s)
+    for m in range(len(s)):
+      q = fx_b_cf[m]
+      for d in iter_stirling(s, m+1):
+        d = map(sum, d)
+        fx_b[k] += functools.reduce(operator.mul, (c[k] for k in d)) * q
+
+  return Dual({**fx_b, **{0: fx_a}})
+
+# integral power is derived by brute-force expansion
+# other functions are defined through power-series expansion
+
+def pow_int(x, n):
+  if not isinstance(n, numbers.Integral):
+    raise ValueError('can only raise to integer power')
+  if x.get(0, 0) == 0 and n < 0:
+    raise ZeroDivisionError('can only raise to non-negative integer power')
+  a = x.get(0, 0)
+  r = a**n
+  if n >= 0:
+    return func_from_series(
+      x, r,
+      lambda m: r * math.perm(n, m),
+      lambda m: math.factorial(n) if m == n else 0)
+  else:
+    # lambda expression can be obtained by taking limit of math.perm(n, m) =
+    # math.gamma(n+1) / math.gamma(n-m+1) at n < 0
+    return func_from_series(
+      x, r,
+      lambda m: r * (-1 if m & 1 else 1) * math.perm(m-n-1, m),
+      None)
+
+def double_factorial(n):
+  return functools.reduce(operator.mul, range(n, 0, -2), 1)
+
+def math_func(lazy_smath_func):
+  def wrap(dual_func):
+    def wrapper(x):
+      if isinstance(x, Dual):
+        return dual_func(x)
+      elif isinstance(x, stype):
+        return lazy_smath_func()(x)
+      else:
+        raise TypeError(
+          'must be {}, not {}'
+          .format(format_types([Dual, *stype]), type(x).__name__))
+    return wrapper
+  return wrap
+
+@math_func(lambda: smath.exp)
+def exp(x):
+  a = x.get(0, 0)
+  r = smath.exp(a)
+  return func_from_series(
+    x, r,
+    lambda m: r * a**m,
+    lambda m: 1)
+
+@math_func(lambda: smath.log)
+def log(x):
+  a = x.get(0, 0)
+  r = smath.log(a)
+  try:
+    c = 1 - 1/a
+  except ZeroDivisionError:
+    raise ValueError('math domain error') from None
+  return func_from_series(
+    x-1, r,
+    lambda m: (1 if m & 1 else -1) * math.factorial(m-1) * c**m,
+    lambda m: (1 if m & 1 else -1) * math.factorial(m-1))
+
+@math_func(lambda: smath.sin)
+def sin(x):
+  a = x.get(0, 0)
+  c, s = smath.cos(a), smath.sin(a)
+  return func_from_series(
+    x, s,
+    lambda m: (-1 if m & 2 else 1) * (c if m & 1 else s) * a**m,
+    lambda m: (-1 if m & 2 else 1) * (m & 1))
+
+@math_func(lambda: smath.cos)
+def cos(x):
+  a = x.get(0, 0)
+  c, s = smath.cos(a), smath.sin(a)
+  return func_from_series(
+    x, c,
+    lambda m: (-1 if m+1 & 2 else 1) * (s if m & 1 else c) * a**m,
+    lambda m: (-1 if m+1 & 2 else 1) * (m+1 & 1))
+
+@math_func(lambda: smath.tan)
+def tan(x):
+  return sin(x) / cos(x)
+
+@math_func(lambda: smath.asin)
+def asin(x):
+  a = x.get(0, 0)
+  return func_from_series(
+    x, smath.asin(a),
+    lambda m:
+      math.factorial(m-1)**2 * a**(m*2-1) / (1-a**2)**(m-sfrac(1, 2)) *
+      sum((a*2)**-l / math.factorial(m-l-1) / math.factorial(l>>1)**2
+        for l in range(0, m, 2)),
+    lambda m: double_factorial(m-2)**2 if m & 1 else 0)
+
+@math_func(lambda: smath.acos)
+def acos(x):
+  # smath.pi/2 - asin(x)
+  y = -asin(x)
+  y[0] = smath.acos(x.get(0, 0))
+  return y
+
+@math_func(lambda: smath.atan)
+def atan(x):
+  return asin(x / hypot(1, x))
+
+@math_func(lambda: smath.sinh)
+def sinh(x):
+  a = x.get(0, 0)
+  c, s = smath.cosh(a), smath.sinh(a)
+  return func_from_series(
+    x, smath.sinh(a),
+    lambda m: (c if m & 1 else s) * a**m,
+    lambda m: m & 1)
+
+@math_func(lambda: smath.cosh)
+def cosh(x):
+  a = x.get(0, 0)
+  c, s = smath.cosh(a), smath.sinh(a)
+  return func_from_series(
+    x, smath.cosh(a),
+    lambda m: (s if m & 1 else c) * a**m,
+    lambda m: m+1 & 1)
+
+@math_func(lambda: smath.tanh)
+def tanh(x):
+  return sinh(x) / cosh(x)
+
+@math_func(lambda: smath.asinh)
+def asinh(x):
+  return log(x + hypot(1, x))
+
+@math_func(lambda: smath.acosh)
+def acosh(x):
+  return log(x + sqrt(x-1) * sqrt(x+1))
+
+@math_func(lambda: smath.atanh)
+def atanh(x):
+  return (log(1+x) - log(1-x)) / 2
+
+def format_alts(alts):
+  alts = list(map(str, alts))
+  if len(alts) == 0:
+    return ''
+  elif len(alts) == 1:
+    return alts[0]
+  elif len(alts) == 2:
+    return '{} or {}'.format(*alts)
+  else:
+    return ', '.join(alts[:-1]) + ', or ' + alts[-1]
+
+def format_types(types):
+  return format_alts(type.__name__ for type in types)
---
+++ cross-nonzero-alt/test.py
@@ -0,0 +1,439 @@
+import functools
+import itertools
+import operator
+import unittest
+
+import dual
+
+@functools.lru_cache(maxsize=None)
+def stirling(n, k):
+  # [[https://en.wikipedia.org/wiki/Stirling_numbers_of_the_second_kind]]
+  if n == 0 and k == 0:
+    return 1
+  elif n == 0 or k == 0:
+    return 0
+  else:
+    return stirling(n-1, k-1) + stirling(n-1, k) * k
+
+class IterationTest(unittest.TestCase):
+  max_ball_count = 12
+  max_box_count  = 50
+
+  def test_stirling(self):
+    for n in range(self.max_ball_count+1):
+      for k in range(self.max_box_count+1):
+        d = dual.iter_stirling(range(n), k)
+        if (n > 0 and k == 0) or n < k:
+          with self.assertRaises(StopIteration):
+            next(d)
+        else:
+          r = set()
+          for d in d:
+            self.assertEqual(len(d), k)
+            self.assertTrue(all(d))
+            self.assertCountEqual(itertools.chain.from_iterable(d), range(n))
+            r.add(tuple(map(tuple, d)))
+          self.assertEqual(len(r), stirling(n, k))
+
+class DualTest:
+  @staticmethod
+  def format_param(*x):
+    return 'parameters are {!r}'.format(x)
+
+class DualExactTest(DualTest):
+  def test_add_asso(self):
+    for x, y, z in self.sample(3):
+      self.assertEqual((x+y)+z, x+(y+z), self.format_param(x, y, z))
+
+  def test_add_comm(self):
+    for x, y in self.sample(2, allow_repeats=False):
+      self.assertEqual(x+y, y+x, self.format_param(x, y))
+
+  def test_add_iden(self):
+    for x, in self.sample():
+      self.assertEqual(x+0, x, self.format_param(x))
+      self.assertEqual(0+x, x, self.format_param(x))
+
+  def test_add_sub_inv(self):
+    for x, y in self.sample(2):
+      self.assertEqual(x+y-y, x, self.format_param(x, y))
+      self.assertEqual(x-y+y, x, self.format_param(x, y))
+
+  def test_mul_asso(self):
+    for x, y, z in self.sample(3):
+      self.assertEqual((x*y)*z, x*(y*z), self.format_param(x, y, z))
+
+  def test_mul_comm(self):
+    for x, y in self.sample(2, allow_repeats=False):
+      self.assertEqual(x*y, y*x, self.format_param(x, y))
+
+  def test_mul_iden(self):
+    for x, in self.sample():
+      self.assertEqual(x*1, x, self.format_param(x))
+      self.assertEqual(1*x, x, self.format_param(x))
+
+  def test_truediv_zero(self):
+    for x, y in self.sample(2):
+      if y[0] == 0:
+        with self.assertRaises(ZeroDivisionError):
+          x/y
+
+  def test_mul_truediv_inv(self):
+    for x, y in self.sample(2):
+      if y[0] != 0:
+        self.assertEqual(x*y/y, x, self.format_param(x, y))
+        self.assertEqual(x/y*y, x, self.format_param(x, y))
+
+  def test_add_mul_dist(self):
+    for x, y, z in self.sample(3):
+      self.assertEqual(x*(y+z), x*y+x*z, self.format_param(x, y, z))
+      self.assertEqual((y+z)*x, y*x+z*x, self.format_param(x, y, z))
+
+  def test_pow_zero(self):
+    for x, in self.sample():
+      self.assertEqual(x**0, 1, self.format_param(x))
+
+  def test_pow_pos(self):
+    for x, in self.sample():
+      y = 1
+      for n in range(1, self.max_pow+1):
+        y *= x
+        self.assertEqual(x**n, y, self.format_param(x, n))
+
+  def test_pow_neg(self):
+    for x, in self.sample():
+      if x[0] != 0:
+        y = 1
+        for n in range(1, self.max_pow+1):
+          y /= x
+          self.assertEqual(x**-n, y, self.format_param(x, n))
+
+  def test_exp_neg(self):
+    for x, in self.sample():
+      self.assertEqual(dual.exp(-x), 1/dual.exp(x), self.format_param(x))
+
+  def test_log_zero(self):
+    for x, in self.sample():
+      if x[0] == 0:
+        with self.assertRaises(ValueError):
+          dual.log(x)
+
+  def test_exp_log_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.exp, dual.log, x)
+
+  def test_sin_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.sin(-x), -dual.sin(x), self.format_param(x))
+
+  def test_sin_asin_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.sin, dual.asin, x)
+
+  def test_cos_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.cos(-x), dual.cos(x), self.format_param(x))
+
+  def test_cos_acos_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.cos, dual.acos, x)
+
+  def test_tan_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.tan(-x), -dual.tan(x), self.format_param(x))
+
+  def test_tan_atan_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.tan, dual.atan, x)
+
+  def test_sin_cos_thm(self):
+    for x, in self.sample():
+      self.assertEqual(
+        dual.cos(x)**2 + dual.sin(x)**2, 1, self.format_param(x))
+
+  def test_sin_cos_tan_thm(self):
+    for x, in self.sample():
+      self.assertEqual(
+        dual.sin(x) / dual.cos(x), dual.tan(x), self.format_param(x))
+
+  def test_sinh_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.sinh(-x), -dual.sinh(x), self.format_param(x))
+
+  def test_sinh_asinh_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.sinh, dual.asinh, x)
+
+  def test_cosh_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.cosh(-x), dual.cosh(x), self.format_param(x))
+
+  def test_cosh_acosh_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.cosh, dual.acosh, x)
+
+  def test_tanh_sym(self):
+    for x, in self.sample():
+      self.assertEqual(dual.tanh(-x), -dual.tanh(x), self.format_param(x))
+
+  def test_tanh_atanh_inv(self):
+    for x, in self.sample():
+      self.assert_inv(dual.tanh, dual.atanh, x)
+
+  def test_sinh_cosh_thm(self):
+    for x, in self.sample():
+      self.assertEqual(
+        dual.cosh(x)**2 - dual.sinh(x)**2, 1, self.format_param(x))
+
+  def test_sinh_cosh_tanh_thm(self):
+    for x, in self.sample():
+      self.assertEqual(
+        dual.sinh(x) / dual.cosh(x), dual.tanh(x), self.format_param(x))
+
+  def test_hash(self):
+    for x, in self.sample():
+      y = dual.Dual({**dict.fromkeys(range(max(x.keys()) + 1), 0), **x})
+      z = dual.Dual({0: x[0]})
+      super().assertEqual(hash(x), hash(y))
+      super().assertEqual(hash(z), hash(z[0]))
+
+  def assert_inv(self, f, i, x):
+    def collapse_dual(x):
+      return dual.Dual({**x, **{0: self.collapse_scalar(x[0])}})
+    y = f(x)
+    if self.valid_for(i, y):
+      self.assertEqual(collapse_dual(i(y)), x)
+    if self.valid_for(i, x):
+      self.assertEqual(collapse_dual(f(i(x))), x)
+
+try:
+  import sympy
+except ImportError:
+  has_sympy = False
+else:
+  has_sympy = True
+
+@unittest.skipUnless(has_sympy, 'requires SymPy')
+class DualSymbolTest(DualExactTest, unittest.TestCase):
+  unit_count = 3
+  term_count = 1 << unit_count
+  max_pow    = 16
+
+  @classmethod
+  def setUpClass(cls):
+    cls.duals = []
+
+    def make_dual(symbol):
+      return dual.Dual(enumerate(
+        sympy.symbols('{}:{}'.format(symbol, cls.term_count))))
+
+    for symbol in 'abc':
+      cls.duals.append(make_dual(symbol))
+
+    cls.zero = make_dual('z')
+    cls.zero[0] = 0
+
+  def setUp(self):
+    dual.use_scalar('symbol')
+
+  def tearDown(self):
+    dual.use_scalar('real')
+
+  def assertEqual(self, x, y, msg=None):
+    x -= y
+    x = dual.Dual({k: sympy.simplify(v) for k, v in x.items()})
+    super().assertEqual(x, 0, msg)
+
+  def test_pow_inv(self):
+    for x, y in self.sample(2):
+      if x[0] != 0 and y[0] != 0:
+        x, y = +x, +y
+        x[0], _ = sympy.posify(x[0])
+        y[0], _ = sympy.posify(y[0])
+        self.assertEqual((x**y)**(1/y), x, self.format_param(x, y))
+        self.assertEqual((x**(1/y))**y, x, self.format_param(x, y))
+
+  def test_log_rcp(self):
+    for x, in self.sample():
+      if x[0] != 0:
+        x = +x
+        x[0], _ = sympy.posify(x[0])
+        self.assertEqual(dual.log(1/x), -dual.log(x), self.format_param(x))
+
+  def test_asin_log(self):
+    for x, in self.sample():
+      y = dual.asin(x)
+      y[0] = y[0].subs(
+        sympy.asin(x[0]), self.asin_to_log(sympy.sqrt, sympy.log, x[0]))
+      z = self.asin_to_log(dual.sqrt, dual.log, x)
+      self.assertEqual(y, z, self.format_param(x))
+
+  def test_acos_log(self):
+    for x, in self.sample():
+      y = dual.acos(x)
+      y[0] = y[0].subs(
+        sympy.acos(x[0]), self.acos_to_log(sympy.sqrt, sympy.log, x[0]))
+      z = self.acos_to_log(dual.sqrt, dual.log, x)
+      self.assertEqual(y, z, self.format_param(x))
+
+  def sample(self, n=1, *, allow_repeats=True):
+    yield self.duals[:n]
+    for i in range(n):
+      yield [self.duals[j] if i != j else self.zero for j in range(n)]
+
+  @staticmethod
+  def valid_for(i, x):
+    if i is dual.log:
+      return x[0] != 0
+
+  @staticmethod
+  def collapse_scalar(x):
+    return sympy.simplify(x, inverse=True)
+
+  @staticmethod
+  def asin_to_log(sqrt, log, x):
+    from sympy import I
+    return -I * log(sqrt(1-x**2) + I*x)
+
+  @staticmethod
+  def acos_to_log(sqrt, log, x):
+    from sympy import I
+    return -I * log(I*sqrt(1-x**2) + x)
+
+import math
+import random
+import sys
+
+epsilon = sys.float_info.epsilon
+sqrt_epsilon = math.sqrt(epsilon)
+
+class DualNumberTest(DualTest):
+  pure_count     = 4
+
+  unit_count     = 32
+  unit_zero_frac = 1/8
+
+  mix_count      = 32
+  max_fctr_count = 2
+  max_term_count = 4
+  mix_zero_frac  = 1/8
+
+  @classmethod
+  def setUpClass(cls):
+    pures = [cls.zero, cls.one]
+    pures += [dual.Dual({0: cls.random()}) for _ in range(cls.pure_count)]
+
+    units = [
+      dual.Dual.new(cls.random(), cls.random())
+      for _ in range(cls.unit_count)]
+    unit_keys = list(set(k for x in units for k in x.keys()))
+    unit_keys.remove(0)
+
+    mixes = []
+    for _ in range(cls.mix_count):
+      while True:
+        fctr_count = random.randint(1, cls.max_fctr_count)
+        term_count = random.randint(1, cls.max_term_count)
+        if fctr_count != 1 or term_count != 1:
+          break
+      mixes.append(dual.Dual({
+        **{0: cls.random()},
+        **{functools.reduce(operator.or_, random.sample(unit_keys, fctr_count)):
+        cls.random() for _ in range(term_count)}}))
+
+    for x in random.sample(units, round(cls.unit_count * cls.unit_zero_frac)):
+      x[0] = 0
+    for x in random.sample(mixes, round(cls.mix_count * cls.mix_zero_frac)):
+      x[0] = 0
+
+    cls.duals = pures + units + mixes
+
+  def sample(self, n=1, *, allow_repeats=True):
+    if allow_repeats:
+      return itertools.product(self.duals, repeat=n)
+    else:
+      return itertools.combinations(self.duals, n)
+
+class DualFloatTest(DualNumberTest):
+  series_term_count = 32
+
+  series_term_max = series_term_count * epsilon**(1/series_term_count) / math.e
+
+  def assertAlmostEqual(self, x, y, msg=None):
+    if not dual.isclose(x, y, abs_tol=sqrt_epsilon):
+      std = '{!r} != {!r} in approximate sense'.format(x, y)
+      msg = self._formatMessage(msg, std)
+      raise self.failureException(msg)
+
+  def test_exp_series(self):
+    for x, in self.sample():
+      self.assertAlmostEqual(
+        dual.exp(x),
+        sum(
+          x**n / math.factorial(n)
+          for n in range(self.series_term_count)),
+        self.format_param(x))
+
+  def test_sin_series(self):
+    for x, in self.sample():
+      self.assertAlmostEqual(
+        dual.sin(x),
+        sum(
+          (-1 if n & 1 else 1) * x**(2*n+1) / math.factorial(2*n+1)
+          for n in range(self.series_term_count)),
+        self.format_param(x))
+
+  def test_cos_series(self):
+    for x, in self.sample():
+      self.assertAlmostEqual(
+        dual.cos(x),
+        sum(
+          (-1 if n & 1 else 1) * x**(2*n) / math.factorial(2*n)
+          for n in range(self.series_term_count)),
+        self.format_param(x))
+
+  def test_sinh_series(self):
+    for x, in self.sample():
+      self.assertAlmostEqual(
+        dual.sinh(x),
+        sum(
+          x**(2*n+1) / math.factorial(2*n+1)
+          for n in range(self.series_term_count)),
+        self.format_param(x))
+
+  def test_cosh_series(self):
+    for x, in self.sample():
+      self.assertAlmostEqual(
+        dual.cosh(x),
+        sum(
+          x**(2*n) / math.factorial(2*n)
+          for n in range(self.series_term_count)),
+        self.format_param(x))
+
+class DualRealTest(DualFloatTest, unittest.TestCase):
+  zero = dual.Dual({0: 0})
+  one  = dual.Dual({0: 1})
+
+  @classmethod
+  def random(cls):
+    return (
+      2**random.uniform(
+        math.log2(sqrt_epsilon), math.log2(cls.series_term_max)) *
+      random.choice([-1, 1]))
+
+class DualComplexTest(DualFloatTest, unittest.TestCase):
+  zero = dual.Dual({0: 0})
+  one  = dual.Dual({0: 1})
+
+  def setUp(self):
+    dual.use_scalar('complex')
+
+  def tearDown(self):
+    dual.use_scalar('real')
+
+  @classmethod
+  def random(cls):
+    return complex(DualRealTest.random(), DualRealTest.random())
+
+if __name__ == '__main__':
+  unittest.main()
